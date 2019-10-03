4980a0fc1b6e77df957a3f8f53ff1b705b1ccf6a
alan <alan@compendiumdev.co.uk>
2019-10-03 16:41:59 +0100

    removed RestAssured
     as only 1 HTTP call being made in this code, so no need for big library

--- src/main/java/uk/co/compendiumdev/drillreporting/api/DrillQuery.java
+++ src/main/java/uk/co/compendiumdev/drillreporting/api/DrillQuery.java
@@ -1,22 +1,35 @@
 package uk.co.compendiumdev.drillreporting.api;
 
 import com.google.gson.Gson;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import uk.co.compendiumdev.drillreporting.outputformats.FormatResponseTableAsMarkdownTable;
+
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DrillQuery {
     private final String bodyContent;
     private final String url;
+    URL requestUrl;
 
     public DrillQuery(final String bodyContent, final String url) {
 
         this.bodyContent = bodyContent;
         this.url = url;
+        try {
+            requestUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
-    // TODO remove REST Assured for this request
     public DrillQueryResult post() {
+
+        if(requestUrl==null){
+            throw new RuntimeException("URL not set correctly");
+        }
 
         final DrillQueryResult result = new DrillQueryResult();
 
@@ -26,19 +39,50 @@ public class DrillQuery {
         result.setUrl(url);
         System.out.println(result.url());
 
-        final Response response = RestAssured.given().header("Content-Type", "application/json").
-                body(body).when().post(url).andReturn();
 
-        result.setStatusCode(response.statusCode());
-        result.setResponseBody(response.body().asString());
+        try {
+            HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("User-Agent", "drill-reporter");
+            connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            OutputStream out = connection.getOutputStream();
+            out.write(body.getBytes());
+
+            int statusCode = connection.getResponseCode();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer responseBody = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                responseBody.append(inputLine);
+            }
+            in.close();
+
+            result.setStatusCode(statusCode);
+            result.setResponseBody(responseBody.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        final Response response = RestAssured.given().header("Content-Type", "application/json").
+//                body(body).when().post(url).andReturn();
+//
+//        result.setStatusCode(response.statusCode());
+//        result.setResponseBody(response.body().asString());
 
         System.out.println(result.statusCode());
         System.out.println(result.responseBody());
 
         if(result.statusCode()==200){
-            result.setMarkdown(formatResponseTableAsMarkdown(response.body().asString()));
+            result.setMarkdown(formatResponseTableAsMarkdown(result.responseBody()));
             System.out.println(result.markdown());
-            result.setNumberOfRows(countNumberOfResults(response.body().asString()));
+            result.setNumberOfRows(countNumberOfResults(result.responseBody()));
         }
 
         return result;
